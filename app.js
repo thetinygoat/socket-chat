45916d34d8ca2020f066df896f880a6aa656ed68
Sachin Saini <iamsainisachin@gmail.com>
2018-07-22 23:15:42 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+// app.set('view engine', 'pug')
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname+'/views/index.html')
+});
+
+io.on('connection', (socket)=>{
+    socket.on('chat message',(data)=>{
+        io.emit('chat message', data);
+    });
+    socket.on('disconnect', ()=>{
+        io.emit('disconnect', 'user disconnected!');
+    });
+    socket.on('typing', (data)=>{
+        io.emit('typing', data);
+    });
+    socket.on('notyping', (data) => {
+        io.emit('notyping', data);
+    })
+});
+
+http.listen(8080,()=>{
+    console.log('server started on port 8080');
+})
